9c83db0e5a40d61e3c260a28652d6bfd1092d7d2
Bobcat <borislav_bogoev@students.softuni.bg>
2026-01-27 20:32:10 +0200

    Additional things added to set up

--- Petstagram/settings.py
+++ Petstagram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 PROJECT_APPS = ['accounts', 'pets', 'common', 'photos']
 INSTALLED_APPS = [
+     'unfold',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
